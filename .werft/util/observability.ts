680510db724e626183958704f09b74afcb5a9041
ArthurSens <arthursens2005@gmail.com>
2021-09-14 14:19:22 +0000

    add cloning credentials
    Signed-off-by: ArthurSens <arthursens2005@gmail.com>

--- .werft/util/observability.ts
+++ .werft/util/observability.ts
@@ -12,5 +12,6 @@ import { werft, exec } from './shell';
 
 export async function installMonitoringSatellite(params: InstallMonitoringSatelliteParams) {
     werft.log('monitoring-satellite', `cloning observability repository`)
-    exec('git clone https://github.com/gitpod-io/observability')
+    exec('git clone https://gitpod-gitops-bot:$(cat /mnt/secrets/gitpod-bot-github-token/token)@github.com/gitpod-io/observability')
+    exec('cd observability && make setup-workspace')
 }
